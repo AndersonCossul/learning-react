d751aa0e2149608ef6e1e47768e184bf5f89fab0
Anderson Cossul <anderson_cossul@hotmail.com>
2017-12-15 22:42:37 -0200

    DeletePersonHandler

--- src/App.js
+++ src/App.js
@@ -24,14 +24,10 @@ class App extends Component {
     })
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons
+    persons.splice(personIndex, 1)
+    this.setState({persons: persons})
   }
 
   togglePersonsHandler = () => {
@@ -54,13 +50,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return (<Person
-              return name={person.name}
-              age={person.age}
-              click={this.switchNameHandler.bind(this, 'Anderson!')}
-              changed={this.nameChangeHandler}/>
-            )
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}/>
           })}
         </div>
       )
