9628999a22637523df22a7d48315bdc4c0d1ec34
Anderson Cossul <anderson_cossul@hotmail.com>
2017-12-15 22:06:46 -0200

    Cleaned master from first_assignment tasks.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import UserOutput from './UserOutput/UserOutput'
-import UserInput from './UserInput/UserInput'
-
 class App extends Component {
-  state = {
-    username: 'Anderson'
-  }
-
-  usernameHandler = (event) => {
-    this.setState({username: event.target.value})
-  }
-
   render() {
     return (
       <div className="App">
-        <UserInput
-          changed={this.usernameHandler}
-          username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
-        <UserOutput username={this.state.username}/>
+        
       </div>
     );
   }
