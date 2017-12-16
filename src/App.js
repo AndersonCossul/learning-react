2c6fa9b10314e4a912a84fbcedf9c518d7b6c3c0
Anderson Cossul <anderson_cossul@hotmail.com>
2017-12-16 19:36:10 -0200

    Media query for person.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium'
+import Radium, { StyleRoot } from 'radium'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -18,21 +18,21 @@ class App extends Component {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id
     })
-  
+
     // working with a copy, not reference
-    const person = {...this.state.persons[personIndex]}
+    const person = { ...this.state.persons[personIndex] }
     person.name = event.target.value
 
     const persons = [...this.state.persons]
     persons[personIndex] = person
 
-    this.setState( {persons: persons} )
+    this.setState({ persons: persons })
   }
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons]
     persons.splice(personIndex, 1)
-    this.setState({persons: persons})
+    this.setState({ persons: persons })
   }
 
   togglePersonsHandler = () => {
@@ -67,7 +67,7 @@ class App extends Component {
               name={person.name}
               age={person.age}
               changed={(event) => this.nameChangedHandler(event, person.id)}
-              key={person.id}/>
+              key={person.id} />
           })}
         </div>
       )
@@ -80,20 +80,22 @@ class App extends Component {
     if (this.state.persons.length <= 2) {
       classes.push('red')
     }
-    
+
     if (this.state.persons.length <= 1) {
       classes.push('bold')
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App!</h1>
-        <p className={classes.join(' ')}>This is really working</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App!</h1>
+          <p className={classes.join(' ')}>This is really working</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
