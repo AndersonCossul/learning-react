c4703c77aafe5d24c10644b1ece173bfa6bb3798
Anderson Cossul <anderson_cossul@hotmail.com>
2017-12-15 22:51:52 -0200

    Added key prop to list.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 0, name: 'Max', age: 28 },
+      { id: 1, name: 'Manu', age: 29 },
+      { id: 2, name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -17,15 +17,15 @@ class App extends Component {
   switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
+        { id: 0, name: newName, age: 28 },
+        { id: 1, name: 'Manu', age: 29 },
+        { id: 2, name: 'Stephanie', age: 27 }
       ]
     })
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons
+    const persons = [...this.state.persons] // make a copy, not reference
     persons.splice(personIndex, 1)
     this.setState({persons: persons})
   }
@@ -54,7 +54,8 @@ class App extends Component {
             return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age}/>
+              age={person.age}
+              key={person.id}/>
           })}
         </div>
       )
